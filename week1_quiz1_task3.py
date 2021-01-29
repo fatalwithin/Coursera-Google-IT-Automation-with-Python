abdcba981158a2cb6aab3b1acfedde1bf7738139
Стрельников Андрей Андреевич <Strelnikov.A.Andree@sberbank.ru>
2021-01-29 16:41:07 +0300

    practice 1

---
+++ week1_quiz1_task3.py
@@ -0,0 +1,23 @@
+import re
+import datetime
+
+def compare_strings(string1, string2):
+  #Convert both strings to lowercase 
+  #and remove leading and trailing blanks
+  string1 = string1.lower().strip()
+  string2 = string2.lower().strip()
+
+  #Ignore punctuation
+  punctuation = r"[.?!,;:-']" #here you should screen the dash symbol as it turns left and right characters into an interval 
+  string1 = re.sub(punctuation, r"", string1)
+  string2 = re.sub(punctuation, r"", string2)
+
+  #DEBUG CODE GOES HERE
+  print("string1 = {}; string2 = {}".format(string1, string2))
+
+  return string1 == string2
+
+print(compare_strings("Have a Great Day!", "Have a great day?")) #True
+print(compare_strings("It's raining again.", "its raining, again")) #True
+print(compare_strings("Learn to count: 1, 2, 3.", "Learn to count: one, two, three.")) #False
+print(compare_strings("They found some body.", "They found somebody.")) #False
