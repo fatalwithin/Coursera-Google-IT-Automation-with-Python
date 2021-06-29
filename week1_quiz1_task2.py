d11c232408e836cf0ddbbb5802f330980d6bffd9
Andrey Strelnikov <39986230+fatalwithin@users.noreply.github.com>
2021-06-30 00:38:24 +0300

    Update week1_quiz1_task2.py

--- week1_quiz1_task2.py
+++ week1_quiz1_task2.py
@@ -1,6 +1,9 @@
 """
-Вопрос 2
-The find_item function uses binary search to recursively locate an item in the list, returning True if found, False otherwise. Something is missing from this function. Can you spot what it is and fix it? Add debug lines where appropriate, to help narrow down the problem.
+Question 2
+The find_item function uses binary search to recursively locate an item in the list, 
+returning True if found, False otherwise. Something is missing from this function. 
+Can you spot what it is and fix it? Add debug lines where appropriate, 
+to help narrow down the problem.
 """
 
 def find_item(list, item):
